87fe26fac171946fda7b5c7c1674edd8e01543b2
shital <143158155+1908shital@users.noreply.github.com>
2026-03-19 19:55:26 +0530

    Fix case of database config import path

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 
-const connectDB = require("./config/db");
+const connectDB = require("./config/Db");
 
 const authRoutes = require("./Routes/authRoutes");
 const courseRoutes = require("./Routes/courseRoutes");
